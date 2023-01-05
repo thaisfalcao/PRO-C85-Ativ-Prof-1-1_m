93d8966fb3f01399be7a87f375e1a5b2da75da0d
Thaís Falcão <101199332+thaisfalcao@users.noreply.github.com>
2023-01-05 12:59:42 -0300

    Add files via upload

---
+++ screens/Login.js
@@ -0,0 +1,57 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#5653d4"
+    },
+    bgImage: {
+      flex: 1,
+      resizeMode: "cover",
+      justifyContent: "center"
+    },
+  
+    upperContainer: {
+      flex: 0.5,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    appIcon: {
+      width: 280,
+      height: 280,
+      resizeMode: "contain",
+      marginTop: 80
+    },
+    appName: {
+      width: 130,
+      height: 130,
+      resizeMode: "contain"
+    },
+    lowerContainer: {
+      flex: 0.5,
+      alignItems: "center"
+    },
+    textinput: {
+      width: "75%",
+      height: 55,
+      padding: 10,
+      borderColor: "#FFFFFF",
+      borderWidth: 4,
+      borderRadius: 10,
+      fontSize: 18,
+      color: "#FFFFFF",
+      fontFamily: "Rajdhani_600SemiBold",
+      backgroundColor: "#5653D4"
+    },
+    button: {
+      width: "43%",
+      height: 55,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#F48D20",
+      borderRadius: 15
+    },
+    buttonText: {
+      fontSize: 24,
+      color: "#FFFFFF",
+      fontFamily: "Rajdhani_600SemiBold"
+    }
+  });
